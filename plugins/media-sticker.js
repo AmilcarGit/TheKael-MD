d8a707cb8a4d98a9827bd027f9361142621e88b4
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-07-07 06:53:37 -0500

    Actualizar media-sticker.js

--- plugins/media-sticker.js
+++ plugins/media-sticker.js
@@ -1,7 +1,35 @@
 import baileysPkg from "@whiskeysockets/baileys";
 import sharp from "sharp";
+import webp from "node-webpmux";
 
 const { downloadMediaMessage } = baileysPkg;
+
+const PACK_STICKER = "𝚃𝙷𝙴𝚈𝚄𝙸🦋";
+const AUTOR_STICKER = "© AmilcarGit 2026";
+
+async function agregarMetadataSticker(webpBuffer) {
+  const img = new webp.Image();
+
+  const json = {
+    "sticker-pack-id": "thekael-yui-md",
+    "sticker-pack-name": PACK_STICKER,
+    "sticker-pack-publisher": AUTOR_STICKER,
+    emojis: ["🦋"],
+  };
+
+  const exifAttr = Buffer.from([
+    0x49, 0x49, 0x2a, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57,
+    0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00,
+  ]);
+  const jsonBuffer = Buffer.from(JSON.stringify(json), "utf-8");
+  const exif = Buffer.concat([exifAttr, jsonBuffer]);
+  exif.writeUIntLE(jsonBuffer.length, 14, 4);
+
+  await img.load(webpBuffer);
+  img.exif = exif;
+
+  return await img.save(null);
+}
 
 export default {
   command: ["sticker", "s", "stiker"],
@@ -66,9 +94,16 @@ export default {
         .webp({ quality: 80 })
         .toBuffer();
 
+      let stickerFinal = webpBuffer;
+      try {
+        stickerFinal = await agregarMetadataSticker(webpBuffer);
+      } catch (errMetadata) {
+        console.log("No se pudo agregar metadata al sticker:", errMetadata);
+      }
+
       await sock.sendMessage(
         chatId,
-        { sticker: webpBuffer },
+        { sticker: stickerFinal },
         { quoted: msg }
       );
     } catch (err) {
